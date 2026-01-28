947d580e9c0af27f795ae1222fa8a3ed1a9cb4ec
karunashakar07042021-arch <karunashakar07042021@gmail.com>
2026-01-28 16:39:13 +0530

    Churn Analysis SQL

--- E-Commerce Customer churn db.sql
+++ E-Commerce Customer churn db.sql
@@ -5656,3 +5656,343 @@ INSERT INTO customer_churn(CustomerID,Churn,Tenure,PreferredLoginDevice,CityTier
     (55628,0,1,'Mobile Phone',1,11,'Debit Card','Male',3,2,'Laptop & Accessory',4,'Married',3,1,21,1,2,4,186),
     (55629,0,23,'Computer',3,9,'Credit Card','Male',4,5,'Laptop & Accessory',4,'Married',4,0,15,2,2,9,179),
     (55630,0,8,'Mobile Phone',1,15,'Credit Card','Male',3,2,'Laptop & Accessory',3,'Married',4,0,13,2,2,3,169);
+    
+    SELECT * FROM customer_churn;
+        CREATE TABLE customer_churn1 AS (SELECT * FROM customer_churn);
+
+    SELECT COUNT(*) WarehouseToHome FROM customer_churn WHERE WarehouseToHome IS NULL;
+    
+    SELECT COUNT(*) HourSpendOnApp  FROM customer_churn WHERE HourSpendOnApp IS NULL;
+    
+     SELECT COUNT(*) OrderAmountHikeFromlastYear FROM customer_churn WHERE OrderAmountHikeFromlastYear IS NULL;
+     
+     SELECT COUNT(*) DaySinceLastOrder FROM customer_churn WHERE DaySinceLastOrder IS NULL;
+
+    SELECT ROUND(AVG(WarehouseToHome),2) FROM customer_churn WHERE WarehouseToHome ;
+    
+    -------------- Handling Missing Values and Outliers: --------
+    /*Impute mean  for the following columns, and round off to the nearest integer if 
+required: WarehouseToHome, HourSpendOnApp, OrderAmountHikeFromlastYear, 
+DaySinceLastOrder*/
+
+/*Impute mode for the following columns: Tenure, CouponUsed, OrderCount.*/
+
+SET SQL_SAFE_UPDATES=0;
+UPDATE customer_churn JOIN (
+SELECT 
+        ROUND(AVG(WarehouseToHome), 2) AS avg_WarehouseToHome,
+        ROUND(AVG(HourSpendOnApp), 2) AS avg_HourSpendOnApp,
+        ROUND(AVG(OrderAmountHikeFromlastYear), 2) AS avg_OrderAmountHikeFromlastYear,
+        ROUND(AVG(DaySinceLastOrder), 2) AS avg_DaySinceLastOrder,
+        ROUND(AVG(Tenure), 2) AS avg_Tenure,
+        ROUND(AVG(CouponUsed), 2) AS avg_CouponUsed,
+        ROUND(AVG(OrderCount), 2) AS avg_OrderCount
+    FROM customer_churn)  
+    AVG_TABLE
+    SET  WarehouseToHome = COALESCE(WarehouseToHome,
+    AVG_TABLE.avg_WarehouseToHome
+ ),
+  
+ HourSpendOnApp = COALESCE(HourSpendOnApp,
+ AVG_TABLE.avg_HourSpendOnApp
+ ), 
+ OrderAmountHikeFromlastYear = COALESCE(OrderAmountHikeFromlastYear,
+ AVG_TABLE.avg_OrderAmountHikeFromlastYear
+ ), 
+  DaySinceLastOrder = COALESCE(DaySinceLastOrder,
+  AVG_TABLE.avg_DaySinceLastOrder
+ ), 
+    Tenure = COALESCE(Tenure,
+  AVG_TABLE.avg_Tenure
+ ), 
+      CouponUsed = COALESCE(CouponUsed,
+  AVG_TABLE.avg_CouponUsed
+ ), 
+      OrderCount = COALESCE(OrderCount,
+ AVG_TABLE.avg_OrderCount
+ );
+SET SQL_SAFE_UPDATES=1;
+
+
+/*Handle outliers in the 'WarehouseToHome' column by deleting rows where the 
+values are greater than 100.*/
+
+SET SQL_SAFE_UPDATES=0;
+DELETE  FROM customer_churn WHERE WarehouseToHome >100;
+SET SQL_SAFE_UPDATES=1;
+
+
+/*Dealing with Inconsistencies: 
+➢ Replace occurrences of “Phone” in the 'PreferredLoginDevice' column and 
+“Mobile” in the 'PreferedOrderCat' column with “Mobile Phone” to ensure 
+uniformity. 
+➢ Standardize payment mode values: Replace "COD" with "Cash on Delivery" and 
+"CC" with "Credit Card" in the PreferredPaymentMode column.*/
+
+
+SET SQL_SAFE_UPDATES  = 0;
+UPDATE customer_churn SET PreferredLoginDevice ='Mobile Phone'
+WHERE PreferredLoginDevice='Phone';
+SET SQL_SAFE_UPDATES  = 1;
+
+SET SQL_SAFE_UPDATES  = 0;
+UPDATE customer_churn SET PreferedOrderCat ='Mobile Phone'
+WHERE PreferedOrderCat='Mobile';
+SET SQL_SAFE_UPDATES  = 1;
+
+
+
+SET SQL_SAFE_UPDATES  = 0;
+UPDATE customer_churn SET PreferredPaymentMode ='Credit Card'
+WHERE PreferredPaymentMode='CC';
+SET SQL_SAFE_UPDATES  = 1;
+
+SET SQL_SAFE_UPDATES  = 0;
+UPDATE customer_churn SET PreferredPaymentMode ='Cash on Delivery'
+WHERE PreferredPaymentMode='COD';
+SET SQL_SAFE_UPDATES  = 1;
+
+
+
+                         ------ Data Transformation:-----
+                         
+							/*Column Renaming: 
+➢ Rename the column "PreferedOrderCat" to "PreferredOrderCat". 
+➢ Rename the column "HourSpendOnApp" to "HoursSpentOnApp".*/
+SELECT *  FROM customer_churn;
+
+ALTER TABLE customer_churn RENAME COLUMN PreferedOrderCat TO  PreferredOrderCat;
+ALTER TABLE customer_churn RENAME COLUMN HourSpendOnApp TO  HoursSpentOnApp;
+
+SELECT *  FROM customer_churn; 
+DESC customer_churn;
+
+						/*Creating New Columns:*/
+	 /*Create a new column named ‘ComplaintReceived’ with values "Yes" if the 
+corresponding value in the ‘Complain’ is 1, and "No" otherwise.*/
+
+ALTER TABLE customer_churn ADD ComplaintReceived VARCHAR(10);
+
+    SET SQL_SAFE_UPDATES=0;
+UPDATE customer_churn  SET ComplaintReceived = 'YES'
+WHERE Complain=1;
+	SET SQL_SAFE_UPDATES=1;	
+    
+       SET SQL_SAFE_UPDATES=0;
+UPDATE customer_churn  SET ComplaintReceived = 'NO'
+WHERE Complain=0;
+	SET SQL_SAFE_UPDATES=1;	
+    
+    
+/* Create a new column named 'ChurnStatus'. Set its value to “Churned” if the 
+corresponding value in the 'Churn' column is 1, else assign “Active”. */
+
+ALTER TABLE customer_churn ADD ChurnStatus VARCHAR(10);
+
+      SET SQL_SAFE_UPDATES=0;
+UPDATE customer_churn  SET ChurnStatus = CASE WHEN Churn=1 THEN 'Churned'
+											ELSE 'Active'
+                                            END;
+	SET SQL_SAFE_UPDATES=1;	
+    
+  /*Column Dropping: 
+➢ Drop the columns "Churn" and "Complain" from the table.*/
+
+ALTER  TABLE  customer_churn DROP COLUMN Churn,
+DROP COLUMN Complain;
+
+
+
+                 -------- Data Exploration and Analysis: ------
+		
+ ----- Retrieve the count of churned and active customers from the dataset. ---- 
+ SELECT *  FROM customer_churn;
+ 
+ ----- COUNT OF CHURNED CUSTOMERS -----
+ 
+ SELECT COUNT(*) COUNT_OF_CHURNED_CUSTOMERS FROM CUSTOMER_CHURN
+ WHERE  ChurnStatus='Churned' ;
+ 
+  ----- COUNT OF ACTIVE CUSTOMERS -----
+  SELECT COUNT(*) COUNT_OF_ACTIVE_CUSTOMERS FROM CUSTOMER_CHURN
+ WHERE  ChurnStatus='Active' ;
+ 
+ SELECT  COUNT(*) AS TOTAL_CUSTOMER, COUNT(CASE WHEN ChurnStatus='Churned' THEN 1 END) AS COUNT_OF_CHURNED_CUSTOMERS,
+  COUNT(CASE WHEN ChurnStatus='Active' THEN 1 END) AS COUNT_OF_ACTIVE_CUSTOMERS FROM CUSTOMER_CHURN;
+  
+  -------- Display the average tenure and total cashback amount of customers who churned. -----
+   SELECT *  FROM CUSTOMER_CHURN;
+   
+    SELECT AVG(Tenure) AVERAGE_TENURE,SUM(CashbackAmount) TOTAL_CASHBACK_AMOUNT  FROM CUSTOMER_CHURN
+    WHERE ChurnStatus='Churned' ;
+    
+    ----- Determine the percentage of churned customers who complained. ----
+    USE ecomm;
+WITH CTE AS(
+SELECT COUNT(*) CHURNED_CUSTOMERS_COMPLAIN_RECEIVED FROM CUSTOMER_CHURN
+ WHERE  ChurnStatus='Churned'  AND ComplaintReceived='YES'),
+ CTE1 AS( SELECT COUNT(*) CHURNED_CUSTOMERS FROM CUSTOMER_CHURN
+ WHERE  ChurnStatus='Churned' ),
+ CTE2 AS( (SELECT ROUND((CTE.CHURNED_CUSTOMERS_COMPLAIN_RECEIVED/CTE1.CHURNED_CUSTOMERS)
+ *100,2) AS Percentage_Of_Churned_Customer_Complain  FROM CTE
+ CROSS JOIN CTE1))
+ SELECT * FROM CTE2;
+ 
+ SELECT * FROM  CUSTOMER_CHURN;
+ 
+ 
+SELECT 
+    ROUND((SUM(CASE WHEN ComplaintReceived = 'YES' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)),2) 
+        AS Churned_Complaint_Percentage
+FROM CUSTOMER_CHURN
+WHERE ChurnStatus = 'Churned';
+
+
+
+/* Identify the city tier with the highest number of churned customers whose 
+preferred order category is Laptop & Accessory.*/
+  SELECT *  FROM CUSTOMER_CHURN;
+  
+  SELECT CityTier,COUNT(*) NO_OF_CHURNED_CUSTOMER FROM CUSTOMER_CHURN
+  WHERE  ChurnStatus='Churned' AND PreferredOrderCat='Laptop & Accessory'
+  GROUP BY CityTier
+  ORDER BY NO_OF_CHURNED_CUSTOMER DESC
+  LIMIT 1;
+  
+-----  Identify the most preferred payment mode among active customers.  ---
+
+ SELECT PreferredPaymentMode,COUNT(*) CUSTOMER_COUNT  FROM CUSTOMER_CHURN
+  WHERE  ChurnStatus='Active' 
+  GROUP BY PreferredPaymentMode
+  ORDER BY CUSTOMER_COUNT DESC
+  LIMIT 1;
+  
+/*Calculate the total order amount hike from last year for customers who are single 
+and prefer mobile phones for ordering.*/
+
+SELECT SUM(OrderAmountHikeFromlastYear) AS TOTAL_ORDER_AMOUNT_HIKE FROM CUSTOMER_CHURN
+WHERE MaritalStatus='Single'  AND PreferredOrderCat='Mobile Phone';
+
+ /*Find the average number of devices registered among customers who used UPI as 
+their preferred payment mode.*/
+
+SELECT AVG(NumberOfDeviceRegistered) AS AVG_NO_OF_DEVICE_REGISTERED FROM CUSTOMER_CHURN
+WHERE PreferredPaymentMode='UPI';
+
+-- Determine the city tier with the highest number of customers----
+
+SELECT CityTier,COUNT(*) AS CUSTOMER_COUNT FROM CUSTOMER_CHURN
+GROUP BY CityTier
+ORDER BY CUSTOMER_COUNT DESC
+LIMIT 1;
+
+
+-- Identify the gender that utilized the highest number of coupons --
+SELECT Gender,SUM(CouponUsed) AS NUMBER_OF_COUPONS FROM CUSTOMER_CHURN
+GROUP BY Gender
+ORDER BY NUMBER_OF_COUPONS DESC
+LIMIT 1;
+
+
+/*List the number of customers and the maximum hours spent on the app in each 
+preferred order category.*/
+
+SELECT COUNT(*) AS NUMBER_OF_CUSTOMER , MAX(HoursSpentOnApp) AS MAX_HOURS_SPENT,PreferredOrderCat FROM CUSTOMER_CHURN
+GROUP BY PreferredOrderCat;
+
+/*Calculate the total order count for customers who prefer using credit cards and 
+have the maximum satisfaction score.*/
+
+
+SELECT SUM(OrderCount) AS TOTAL_ORDER_COUNT,MAX(SatisfactionScore) AS MAX_SATISFACTION_SCORE FROM CUSTOMER_CHURN
+WHERE PreferredPaymentMode='Credit Card';
+
+-- What is the average satisfaction score of customers who have complained? --
+
+SELECT AVG(SatisfactionScore) AS AVG_SATISFACTION_SCORE_COMPLAINED_CUSTOMERS  FROM CUSTOMER_CHURN
+WHERE ComplaintReceived='YES';
+
+-- List the preferred order category among customers who used more than 5 coupons.---
+
+SELECT PreferredOrderCat,COUNT(*) AS CUSTOMER_COUNT FROM CUSTOMER_CHURN
+WHERE CouponUsed>5
+GROUP BY PreferredOrderCat
+ORDER BY CUSTOMER_COUNT;
+
+-- List the top 3 preferred order categories with the highest average cashback amount.--
+
+SELECT PreferredOrderCat,AVG(CashbackAmount) AS AVG_CASH_BACK_AMOUNT FROM CUSTOMER_CHURN
+GROUP BY PreferredOrderCat
+ORDER BY AVG_CASH_BACK_AMOUNT DESC
+LIMIT 3;
+
+/*Find the preferred payment modes of customers whose average tenure is 10 
+months and have placed more than 500 orders.*/ 
+
+SELECT *  FROM CUSTOMER_CHURN;
+
+SELECT PreferredPaymentMode,COUNT(*) AS CUSTOMER_COUNT FROM CUSTOMER_CHURN
+GROUP  BY PreferredPaymentMode
+HAVING AVG(Tenure)>10 AND SUM(OrderCount)>500;
+
+/*Categorize customers based on their distance from the warehouse to home such 
+as 'Very Close Distance' for distances <=5km, 'Close Distance' for <=10km, 
+'Moderate Distance' for <=15km, and 'Far Distance' for >15km. Then, display the 
+churn status breakdown for each distance category. */
+
+
+
+SELECT 
+ CASE WHEN WarehouseToHome <=5 THEN 'Very Close Distance'
+	  WHEN WarehouseToHome <=10 THEN 'Close Distance'
+       WHEN WarehouseToHome <=15 THEN 'Moderate Distance'
+        WHEN WarehouseToHome >15 THEN 'Far Distance'
+        END AS DISTANCE_CATEGORY,
+        ChurnStatus
+	FROM CUSTOMER_CHURN;
+    
+    
+/*List the customer’s order details who are married, live in City Tier-1, and their 
+order counts are more than the average number of orders placed by all 
+customers.*/
+
+ SELECT * FROM CUSTOMER_CHURN 
+ WHERE MaritalStatus='Married'
+AND CityTier=1 AND OrderCount> (SELECT AVG(OrderCount)  FROM CUSTOMER_CHURN) ;
+
+
+
+
+ /*Create a ‘customer_returns’ table in the ‘ecomm’ database and insert the 
+following data:*/
+
+CREATE TABLE IF  NOT EXISTS CUSTOMER_RETURNS (
+												ReturnID INT,
+                                                CustomerID INT,
+                                                ReturnDate DATE,
+                                                RefundAmount INT,
+                                                PRIMARY KEY (ReturnID, CustomerID)
+											);
+                                            
+                                       
+	INSERT INTO CUSTOMER_RETURNS VALUES  
+    (1001,50022,'2023-01-01',2130),
+    (1002,50316,'2023-01-23',2000),
+    (1003,51099,'2023-02-14',2290),
+    (1004,52321,'2023-03-08',2510),
+    (1005,52928,'2023-03-20',3000),
+    (1006,53749,'2023-04-17',1740),
+    (1007,54206,'2023-04-21',3250),
+    (1008,54838,'2023-04-30',1990);
+    
+    /*Display the return details along with the customer details of those who have 
+churned and have made complaints.*/
+
+SELECT * FROM CUSTOMER_CHURN;
+SELECT * FROM CUSTOMER_RETURNS;
+
+SELECT CR.ReturnID,CR.ReturnDate,CR.RefundAmount,CC.* FROM   CUSTOMER_CHURN CC 
+INNER JOIN CUSTOMER_RETURNS CR 
+ON CC.CustomerID = CR.CustomerID
+WHERE CC.ChurnStatus = 'Churned' 
+AND ComplaintReceived = 'YES';
